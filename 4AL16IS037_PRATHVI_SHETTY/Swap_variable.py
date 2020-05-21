9286f31ee9ae5d9da4d4f7ce55a23489b1f0712f
prathvipshetty <prathvishetty22@gmail.com>
2020-05-21 17:22:50 +0530

    DAY3

---
+++ 4AL16IS037_PRATHVI_SHETTY/Swap_variable.py
@@ -0,0 +1,11 @@
+#4)Python program to swap two variables
+
+x = 5
+y = 10
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
