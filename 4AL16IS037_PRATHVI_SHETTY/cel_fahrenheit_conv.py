5eba20c564cbddc33a3d08ed7fb10f8a93aaa35d
prathvipshetty <prathvishetty22@gmail.com>
2020-05-25 16:31:39 +0530

    PYTHON CODE

---
+++ 4AL16IS037_PRATHVI_SHETTY/cel_fahrenheit_conv.py
@@ -0,0 +1,6 @@
+# Python Program to convert temperature in celsius to fahrenheit
+
+celsius = 37.5
+
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
